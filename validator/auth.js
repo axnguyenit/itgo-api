bee6771d2d56935ba9de2500beffe65bbf6ec83f
khanguyen01it <kha.nguyen01.it@gmail.com>
2022-02-28 20:37:09 +0700

    feature/integrate-zoom

--- validator/auth.js
+++ validator/auth.js
@@ -2,9 +2,9 @@ const { check } = require('express-validator');
 
 const validateRegister = [
 	check('firstName', 'First name is require.').notEmpty(),
-	check('firstName', 'First name must be at least 3 characters long.').isLength({ min: 3 }),
+	check('firstName', 'First name must be at least 2 characters long.').isLength({ min: 2 }),
 	check('lastName', 'Last name is require.').notEmpty(),
-	check('lastName', 'Last name must be at least 3 characters long.').isLength({ min: 3 }),
+	check('lastName', 'Last name must be at least 2 characters long.').isLength({ min: 2 }),
 	check('email', 'Email is require.').notEmpty(),
 	check('email', 'Invalid email.').isEmail(),
 	check('password', 'Password is require.').notEmpty(),
